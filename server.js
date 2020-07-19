29db590d38e95d3d96007f4b8ba9c58b5cb10b3a
eddyangang <eddyyang@berkeley.edu>
2020-07-19 02:47:01 -0700

    making good progress Eddy! Keep it up. Dont lose hope on fixing bugs!

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
 const express = require("express");
-const mysql = require("mysql")
+const fs = require("fs")
 var path = require("path");
 
 const app = express()
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname))
 
-app.get("/", (req, res) = {
+app.get("/api/notes", (req, res) => {
+    
+    fs.readFile(path.join(__dirname + "/db/db.json"), (err, data) => {
 
+        const allNotes =JSON.parse(data);
+        if (err) throw err;
+        console.log(allNotes);
+        res.json(allNotes);
+    })
+
+    
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public", "/notes.html"))
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public", "/index.html"))
+})
+
+
+app.post("/api/notes", (req, res) => {
+
+    let notes = fs.readFileSync(path.join(__dirname + "/db/db.json"));
+
+    notes = JSON.parse(notes);
+
+    notes.push(req.body)
+
+    fs.writeFileSync(path.join(__dirname + "/db/db.json"), JSON.stringify(notes))
+
+    res.json(req.body)
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    let id = req.params.id
 })
 
 app.listen(PORT, () => {
